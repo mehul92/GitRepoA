220a56c7f433009e23e413375d1d4cdcae85fd68
Mehul Srivastava <mehul.srivastava@zomato.com>
2022-01-03 17:50:56 +0530

    Added source files

---
+++ Master/Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Master
+//
+//  Created by Mehul Srivastava on 03/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
